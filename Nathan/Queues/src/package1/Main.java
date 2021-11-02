be598dfbe90832febee3e6dae174e201a880e705
Alex Joslin <aljoslin13@yahoo.com>
2021-11-02 12:40:30 -0700

    noah shape lesson

---
+++ Nathan/Queues/src/package1/Main.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new Queue<Integer>();
+        q.push(5);
+        q.push(6);
+        q.push(8);
+        System.out.println(q.peek());
+        q.pop();
+        q.display();
+    }
+}
