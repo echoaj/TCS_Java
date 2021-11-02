d2dad497001351cb6ac039e74bf2740d8b53cf84
echoaj <aljoslin13@yahoo.com>
2021-11-02 12:43:54 -0700

    fiels

---
+++ Noah_Simon/BlockChain/src/com/company/BlockChain.java
@@ -0,0 +1,96 @@
+package com.company;
+
+
+public class BlockChain {
+
+    int length;
+    Block head;
+    Block tail;
+
+    public BlockChain(){
+        head = new Block(null, null, 0.0);
+        tail = head;
+        head.hash = head.get_hash();
+        length = 1;
+    }
+
+    public void append(Block block){
+        block.prevHash = tail.hash;
+        block.prev = tail;
+        block.hash = block.get_hash();
+        tail = block;
+        length++;
+    }
+
+    // TODO for loop that checks if blockchain is valid
+    // 1) It should compare its current hash with its recalculated hash
+    // 2) It should compare its current hash with block's prev hash
+    public boolean isValid(){
+        Block cur = this.tail;
+        boolean valid = true;
+        while(cur != this.head){
+            assert cur != null: "Cur is null";
+            if(!cur.hash.equals(cur.get_hash())){
+                System.out.println("FIRST");
+                valid = false;
+                break;
+            }
+            if (!cur.prevHash.equals(cur.prev.get_hash())) {
+                System.out.println("SECOND");
+                valid = false;
+                break;
+            }
+            cur = cur.prev;
+        }
+        if(!cur.hash.equals(cur.get_hash())){
+            System.out.println("FIRST");
+            valid = false;
+        }
+        return valid;
+    }
+
+    // TODO Proof of work
+    // 1) check if first n character match to an array of n zeros
+    // 2) keep adding nonce value and recalculate hash
+    // 3) increment nonce till hash matches
+
+    public String display(){
+        Block cur = this.tail;
+        String print = "";
+        while(cur != this.head){
+            String temp = cur.from + " to " + cur.destination + " " + cur.timestamp;
+            print = temp + " <- " + print;
+            cur = cur.prev;
+        }
+        String temp = cur.from + " to  " + cur.destination + " " + cur.timestamp;
+        print = temp + " <- " + print;
+        return print;
+    }
+
+    public void displayHash(){
+        Block cur = this.tail;
+        String print = "";
+        while(cur != this.head){
+            String temp = cur.hash;
+            print = temp + " <- " + print;
+            cur = cur.prev;
+        }
+        String temp = cur.hash;
+        print = temp + " <- " + print;
+        System.out.println(print);
+    }
+
+    public void displayPrevHash(){
+        Block cur = this.tail;
+        String print = "";
+        while(cur != this.head){
+            String temp = cur.prevHash;
+            print = temp + " <- " + print;
+            cur = cur.prev;
+        }
+        String temp = cur.prevHash;
+        print = temp + " <- " + print;
+        System.out.println(print);
+    }
+
+}
